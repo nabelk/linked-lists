c68561a84ed1af526b2f5d4bd96c739a87bca6e8
Nabil Khalid <holanabilkhalid@gmail.com>
2023-05-13 21:50:32 +0800

    Add new functions into the class

--- linked-list.js
+++ linked-list.js
@@ -5,6 +5,7 @@ class LinkedList {
         this.head = head;
     }
 
+    // adds a new node containing value to the end of the list
     append(value) {
         const newNode = Node(value);
         if (!this.head) {
@@ -17,8 +18,84 @@ class LinkedList {
         }
         currentNode.nextNode = newNode;
     }
-}
 
-const test = new LinkedList();
-test.append('wassup');
-console.log(test);
+    // adds a new node containing value to the start of the list
+    prepend(value) {
+        const temp = this.head;
+        this.head = Node(value);
+        this.head.nextNode = temp;
+    }
+
+    // returns the total number of nodes in the list
+    size() {
+        let count = 0;
+        let currentNode = this.head;
+        while (currentNode) {
+            count += 1;
+            currentNode = currentNode.nextNode;
+        }
+        return count;
+    }
+
+    // returns the first node in the list
+    headNode() {
+        return this.head;
+    }
+
+    // returns the last node in the list
+    tailNode() {
+        let currentNode = this.head;
+        if (!this.head) {
+            return 'No node';
+        }
+        while (currentNode.nextNode) {
+            currentNode = currentNode.nextNode;
+        }
+        return currentNode;
+    }
+
+    // returns the node at the given index
+    at(index) {
+        let count = 0;
+        let currentNode = this.head;
+        while (currentNode) {
+            count += 1;
+            if (count === index) {
+                return currentNode;
+            }
+            currentNode = currentNode.nextNode;
+        }
+    }
+
+    // removes the last element from the list
+    pop() {
+        if (!this.head) {
+            console.log('List is empty');
+            return;
+        }
+        if (!this.head.nextNode) {
+            this.head = null;
+            return;
+        }
+
+        let secondLastNode = this.head;
+        let lastNode = secondLastNode.nextNode;
+        while (lastNode.nextNode) {
+            secondLastNode = lastNode;
+            lastNode = lastNode.nextNode;
+        }
+        secondLastNode.nextNode = null;
+    }
+
+    // returns true if the passed in value is in the list and otherwise returns false.
+    contains(value) {
+        let currentNode = this.head;
+        while (currentNode) {
+            if (currentNode.value === value) {
+                return true;
+            }
+            currentNode = currentNode.nextNode;
+        }
+        return false;
+    }
+}
